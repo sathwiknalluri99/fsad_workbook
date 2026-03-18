ba6912840d5e38b8529e44cf7deffb55dd91f052
sathwiknalluri99 <2400030410@kluniversity.in>
2026-03-18 12:10:47 +0530

    Added skill8 and skill9

--- skill5/src/main/java/com/klu/autowire/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package com.klu.autowire;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("com.klu.autowire")
-public class AppConfig {
-}
